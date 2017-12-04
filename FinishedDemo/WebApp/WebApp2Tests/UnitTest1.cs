2845a368c20c17b51d3436bb980f495e67449443
Chris Vitko <chris.vitko@ilmservice.com>
2017-12-04 08:51:04 -0600

    unit test

---
+++ FinishedDemo/WebApp/WebApp2Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using WebApp2._0.Services;
+using Xunit;
+
+namespace WebApp2Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var service = new DataService();
+
+            var result = service.GetName();
+
+            Assert.Same("Chris Vitko", result);
+        }
+    }
+}
